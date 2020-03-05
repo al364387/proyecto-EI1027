91cacf105cfe7757b5acb80f8d264c36d262ce0f
al364387 <al364387@uji.es>
2020-03-05 17:25:26 +0100

    Creación de los métodos de las clases ContractDao, ContractRowMapper, ElderlyDao, ElderlyRowMapper, VolunteerAvailabilityDao y VolunteerAvailabilityRowMapper
    Modificación de los tipos de atributo startTime y endTime de int a Time de la clase VolunteerAvailability

--- src/main/java/es/uji/ei1027/majorsacasa/dao/ContractRowMapper.java
+++ src/main/java/es/uji/ei1027/majorsacasa/dao/ContractRowMapper.java
@@ -1,4 +1,23 @@
 package es.uji.ei1027.majorsacasa.dao;
 
-public class ContractRowMapper {
+import es.uji.ei1027.majorsacasa.model.Contract;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ContractRowMapper implements RowMapper<Contract> {
+    public Contract mapRow(ResultSet rs, int i) throws SQLException {
+        Contract contract = new Contract();
+        contract.setNumber(rs.getString("numContract"));
+        contract.setCatering(rs.getBoolean("catering"));
+        contract.setNursing(rs.getBoolean("nursing"));
+        contract.setCleaning(rs.getBoolean("cleaning"));
+        contract.setPrice(rs.getFloat("price"));
+        contract.setStartDate(rs.getDate("startDate"));
+        contract.setEndDate(rs.getDate("endDate"));
+        contract.setServiceNumber(rs.getInt("serviceNumber"));
+        contract.setCifCompany(rs.getString("cifCompany"));
+        return contract;
+    }
 }
