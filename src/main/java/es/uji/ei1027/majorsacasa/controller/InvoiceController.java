c77a03270753bd453e240fbf8429b2d231de0f18
al361883 <al361883@uji.es>
2020-03-05 17:10:01 +0100

    Volunteer y Invoice

--- src/main/java/es/uji/ei1027/majorsacasa/controller/InvoiceController.java
+++ src/main/java/es/uji/ei1027/majorsacasa/controller/InvoiceController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
-
+//Hola
 @Controller
 @RequestMapping("/invoice")
 
