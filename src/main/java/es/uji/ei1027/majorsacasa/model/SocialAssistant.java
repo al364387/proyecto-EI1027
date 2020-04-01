b22e18a188819c3f2d932bd2ec27d5359f78a7dc
al364387 <al364387@uji.es>
2020-04-01 18:18:19 +0200

    Crear asistente social Creación y modificación de clases

---
+++ src/main/java/es/uji/ei1027/majorsacasa/model/SocialAssistant.java
@@ -0,0 +1,73 @@
+package es.uji.ei1027.majorsacasa.model;
+
+public class SocialAssistant {
+    private String dni;
+    private String name;
+    private String surname;
+    private String email;
+    private String username;
+    private String password;
+
+    public SocialAssistant() {
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "SocialAssistant{" +
+                "dni='" + dni + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
