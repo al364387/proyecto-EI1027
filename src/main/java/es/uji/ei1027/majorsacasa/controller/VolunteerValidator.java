b884b1b2a3863f98d10ad821e496bf2880f3105b
al361883 <al361883@uji.es>
2020-05-03 20:08:30 +0200

    Invoice, Invoice Line, Volunteer Validators

---
+++ src/main/java/es/uji/ei1027/majorsacasa/controller/VolunteerValidator.java
@@ -0,0 +1,41 @@
+package es.uji.ei1027.majorsacasa.controller;
+
+import es.uji.ei1027.majorsacasa.model.Volunteer;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.time.LocalDate;
+
+public class VolunteerValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Volunteer.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Volunteer volunteer = (Volunteer) o;
+
+        if(volunteer.getName().trim().equals("")){
+            errors.rejectValue("name", "obligatorio", "Ha de introducirse un valor");
+        }
+
+        if(volunteer.getSurname().trim().equals("")){
+            errors.rejectValue("surname", "obligatorio", "Ha de introducirse un valor");
+        }
+
+        if(volunteer.getUsername().trim().equals("")){
+            errors.rejectValue("username", "obligatorio", "Ha de introducirse un valor");
+        }
+
+        if(volunteer.getPassword().trim().equals("")){
+            errors.rejectValue("username", "obligatorio", "Ha de introducirse un valor");
+        }
+
+        if((volunteer.getBirthdate().getYear() + 18) < LocalDate.now().getYear()){
+            errors.rejectValue("birthdate", "voluntario muy joven", "Es necesario tener 18 años para ser voluntario");
+        }
+
+
+    }
+}
