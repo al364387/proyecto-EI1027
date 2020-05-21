65afb85598f403110408589149746c1967bf065b
al364387 <al364387@uji.es>
2020-05-21 16:35:20 +0200

    Actualización 5.3

--- src/main/java/es/uji/ei1027/majorsacasa/controller/LoginController.java
+++ src/main/java/es/uji/ei1027/majorsacasa/controller/LoginController.java
@@ -14,6 +14,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+class UserValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> cls) {
+        return UserDetails.class.isAssignableFrom(cls);
+    }
+    @Override
+    public void validate(Object obj, Errors errors) {
+        UserDetails userDetails = (UserDetails) obj;
+        if (userDetails.getUsername().trim().equals("")) {
+            errors.rejectValue("username", "obligatori", "Introduzca el nombre de usuario");
+        }
+        if (userDetails.getPassword().trim().equals("")) {
+            errors.rejectValue("password", "obligatori", "Introduzca la contraseña");
+        }
+    }
+}
 
 @Controller
 public class LoginController {
