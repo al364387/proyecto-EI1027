c110bbd01b9a90270c78ab932c3c67fb32ff3002
al364387 <al364387@uji.es>
2020-06-03 10:30:06 +0200

    Actualización diseño Volunteer y Elderly

--- src/main/java/es/uji/ei1027/majorsacasa/controller/VolunteerValidator.java
+++ src/main/java/es/uji/ei1027/majorsacasa/controller/VolunteerValidator.java
@@ -17,15 +17,15 @@ public class VolunteerValidator implements Validator {
         Volunteer volunteer = (Volunteer) o;
 
         if(volunteer.getName().trim().equals("")){
-            errors.rejectValue("name", "Obligatorio", "Ha de introducirse un nombre");
+            errors.rejectValue("name", "Obligatorio", "Es necesario introducir el nombre");
         }
 
         if(volunteer.getSurname().trim().equals("")){
-            errors.rejectValue("surname", "Obligatorio", "Ha de introducirse un apellido");
+            errors.rejectValue("surname", "Obligatorio", "Es necesario introducir los apellidos");
         }
 
         if(volunteer.getBirthdate()==null){
-            errors.rejectValue("birthdate", "Obligatorio", "Ha de introducirse una fecha válida");
+            errors.rejectValue("birthdate", "Obligatorio", "Es necesario introducir la fecha de nacimiento");
         } else {
             if(volunteer.getBirthdate().getYear() + 18 > LocalDate.now().getYear()){
                 errors.rejectValue("birthdate", "voluntario muy joven", "Es necesario tener 18 años para ser voluntario");
@@ -33,19 +33,19 @@ public class VolunteerValidator implements Validator {
         }
 
         if(volunteer.getPhonenumber()==0){
-            errors.rejectValue("phonenumber", "Obligatorio", "Ha de introducirse un teléfono");
+            errors.rejectValue("phonenumber", "Obligatorio", "Es necesario introducir el número de teléfono");
         }
 
         if(volunteer.getAddress().trim().equals("")){
-            errors.rejectValue("address", "Obligatorio", "Ha de introducirse una dirección");
+            errors.rejectValue("address", "Obligatorio", "Es necesario introducir la dirección");
         }
 
         if(volunteer.getUsername().trim().equals("")){
-            errors.rejectValue("username", "Obligatorio", "Ha de introducirse un nombre de usuario");
+            errors.rejectValue("username", "Obligatorio", "Es necesario introducir un nombre de usuario");
         }
 
         if(volunteer.getPassword().trim().equals("")){
-            errors.rejectValue("password", "Obligatorio", "Ha de introducirse una contraseña");
+            errors.rejectValue("password", "Obligatorio", "Es necesario introducir una contraseña");
         }
 
 
